9b44cd65e90f9e600ebd4fa4edbe075d2e16fa0f
foti <foti.hatzis@gmail.com>
2013-11-06 22:44:27 -0600

    A start

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ siteblg
 TODO
 
 1. Fix .gitignore
+2. Fix paths to files to not rely on '..'
+3. Id for articles should be universally unique
--- svr/blog.js
+++ svr/blog.js
@@ -1,14 +1,32 @@
-// Module Dependecies
-var express = require('express'),
-app = express();
+var express = require('express');
+var ArticleProvider = require('../persistence/ArticleProvider').ArticleProvider;
 
-// Config 
-app.configure( function() {
+var app = module.exports = express();
+
+app.configure(function(){
+  app.set('views', '../views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: '../public' }));
+  app.use(app.router);
+  app.use(express.static('../public'));
 });
 
-// Routes
-app.get('/', function(req, res) {
-	res.send('Hello World');
-})
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-app.listen(3030);
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+var articleProvider = new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+});
+
+app.listen(3000);
